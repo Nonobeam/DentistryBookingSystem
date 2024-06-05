77348541911b02154448ba6bcd3b6dce5b4cb1bb
BobJohn69 <minhtuan030304@gmail.com>
2024-06-05 20:39:36 +0700

    Test

--- System/src/main/java/com/example/DentistryManagement/core/user/Client.java
+++ System/src/main/java/com/example/DentistryManagement/core/user/Client.java
@@ -1,7 +1,12 @@
 package com.example.DentistryManagement.core.user;
 
 
+import com.example.DentistryManagement.core.dentistry.Appointment;
+import com.example.DentistryManagement.core.mail.Notification;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -9,26 +14,47 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table(name = "Client")
+@Table(name = "User")
 @Entity
 public class Client implements UserDetails {
     @Id
+    @JsonIgnore
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "clientId")
     private String id;
+    @NotBlank(message = "Firstname must not be empty")
     private String firstName;
+    @NotBlank(message = "Lastname must not be empty")
     private String lastName;
-    private String gender;
-    private String email;
-    private String password;
+    @NotBlank(message = "Phone number must not be empty")
+    @Pattern(regexp = "\\+?[0-9]+", message = "Invalid phone number format")
     private String phone;
+    @NotBlank(message = "Email must not be empty")
+    private String mail;
+    @NotBlank(message = "Password must not be empty")
+    private String name;
+    private String password;
+    private String birthday;
+    private int status;
     @Enumerated(EnumType.STRING)
     private Role role;
+
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "clientDependent")
+    private List<Optional> dependentList;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "clientNotification")
+    private List<Notification> notificationList;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "clientAppointment")
+    private List<Appointment> clientAppointmentList;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
