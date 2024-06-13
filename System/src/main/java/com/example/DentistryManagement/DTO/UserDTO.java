4d78e53c7287735adee31994cae725e121b14142
Nguyen Huu Phuc <nguyenhuuthuc0405@gmail.com>
2024-06-13 21:41:58 +0700

    Remove case sensitive for Thu

--- System/src/main/java/com/example/DentistryManagement/DTO/UserDTO.java
+++ System/src/main/java/com/example/DentistryManagement/DTO/UserDTO.java
@@ -1,7 +1,6 @@
 package com.example.DentistryManagement.DTO;
 
 import com.example.DentistryManagement.core.user.Role;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Email;
@@ -16,16 +15,11 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class UserDTO {
-    private String userID;
-    @NotBlank(message = "Firstname must not be empty")
     private String firstName;
-    @NotBlank(message = "Lastname must not be empty")
     private String lastName;
-    @NotBlank(message = "Phone number must not be empty")
     @Pattern(regexp = "\\+?[0-9]+", message = "Invalid phone number format")
     @Size(min = 10, max = 11, message = "Phone number cannot exceed 11 characters")
     private String phone;
-    @NotBlank(message = "Email must not be empty")
     @Email(message = "Invalid email format")
     private String mail;
     @Enumerated(EnumType.STRING)
