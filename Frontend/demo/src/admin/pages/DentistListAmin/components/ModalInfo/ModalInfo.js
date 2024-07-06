e47259881c61b07705580fa3be45f2e3e481003e
nguyendangkhoa2905 <nguyendangkhoa2905@gmail.com>
2024-07-06 13:07:54 +0700

    fix loi

--- Frontend/demo/src/admin/pages/DentistListAmin/components/ModalInfo/ModalInfo.js
+++ Frontend/demo/src/admin/pages/DentistListAmin/components/ModalInfo/ModalInfo.js
@@ -4,7 +4,7 @@ import { CustomerServices } from '../../../../services/CustomerServer/CustomerSe
 
 const { TextArea } = Input;
 
-export const ModalInfo = ({ open, setOpen, info, showModal }) => {
+export const ModalInfo = ({ open, setOpen, info, setApiData, data }) => {
   const [formData, setFormData] = useState(info);
 
   const handleCancel = () => {
@@ -14,6 +14,7 @@ export const ModalInfo = ({ open, setOpen, info, showModal }) => {
   const handleSave = async () => {
     // Handle save logic here, like sending data to backend
     console.log(formData);
+    setApiData([...data, formData]);
     const response = await CustomerServices.updateCustomer(formData);
     console.log(response);
     setOpen(false);
@@ -21,14 +22,17 @@ export const ModalInfo = ({ open, setOpen, info, showModal }) => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    if (name !== 'mail') {
+      // Ensure 'mail' field is not updated
+      setFormData({ ...formData, [name]: value });
+    }
   };
 
   return (
     <>
       <Modal
         title='Edit Information'
-        open={open}
+        visible={open} // Use 'visible' instead of 'open' for Modal component
         onCancel={handleCancel}
         footer={[
           <Button key='cancel' onClick={handleCancel}>
@@ -52,20 +56,18 @@ export const ModalInfo = ({ open, setOpen, info, showModal }) => {
               onChange={handleChange}
             />
           </Form.Item>
-          <Form.Item label='mail'>
-            <Input name='mail' value={formData.mail} onChange={handleChange} />
+          <Form.Item label='Mail'>
+            <Input
+              name='mail'
+              value={formData.mail}
+              onChange={handleChange}
+              disabled // Make 'mail' field disabled
+            />
           </Form.Item>
           <Form.Item label='Phone'>
             <Input
               name='phone'
               value={formData.phone}
-              onChange={handleChange}
-            />
-          </Form.Item>
-          <Form.Item label='Feedback'>
-            <TextArea
-              name='feedback'
-              value={formData.feedback}
               onChange={handleChange}
             />
           </Form.Item>
