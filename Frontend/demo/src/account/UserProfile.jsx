02a9d817914f0d7933dc3e4b9085100431c5bfc3
namvgse <kelvinnam.2104@gmail.com>
2024-07-11 23:58:26 +0700

    redesign customer

--- Frontend/demo/src/account/UserProfile.jsx
+++ Frontend/demo/src/account/UserProfile.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Form, Input, Button, DatePicker, message, Spin } from "antd";
+import { Form, Input, Button, DatePicker, message, Spin, Card, Row, Col } from "antd";
+import { UserOutlined, PhoneOutlined, MailOutlined, CalendarOutlined } from '@ant-design/icons';
 import dayjs from "dayjs"; 
 import NavBar from "../homepage/Nav";
 
@@ -42,9 +43,9 @@ const UserProfile = () => {
         "http://localhost:8080/user/info/update",
         {
           name: values.name,
-            phone: values.phone,
-            mail: user.mail,
-            birthday: values.birthday.format("YYYY-MM-DD"),
+          phone: values.phone,
+          mail: user.mail,
+          birthday: values.birthday.format("YYYY-MM-DD"),
           id: user.id,
           status: user.status
         },
@@ -63,62 +64,71 @@ const UserProfile = () => {
 
   return (
     <>
-    <NavBar />
-    <div style={{ maxWidth: "600px", margin: "0 auto", padding: "20px", textAlign: "center" }}>
-      <h1>User Profile</h1>
-      {loading ? (
-        <Spin />
-      ) : (
-        <Form
-          form={form}
-          layout="vertical"
-          onFinish={handleUpdate}
-          initialValues={{
-            name: user.name,
-            phone: user.phone,
-            mail: user.mail,
-            birthday: dayjs(user.birthday)
-          }}
+      <NavBar />
+      <div style={{ maxWidth: "600px", margin: "0 auto", padding: "20px" }}>
+        <Card
+          title="Profile"
+          bordered={false}
+          style={{ textAlign: "center" }}
         >
-          <Form.Item
-            name="name"
-            label="Name"
-            rules={[{ required: true, message: "Please enter your name" }]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="phone"
-            label="Phone"
-            rules={[{ required: true, message: "Please enter your phone number" }]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="mail"
-            label="Email"
-            rules={[{ required: true, message: "Please enter your email" }]}
-          >
-            <Input disabled />
-          </Form.Item>
-          <Form.Item
-            name="birthday"
-            label="Birthday"
-            rules={[{ required: true, message: "Please select your birthday" }]}
-          >
-            <DatePicker style={{ width: "100%" }} />
-          </Form.Item>
-          <Form.Item>
-          <a href="/forgot">Wanna change password?</a>
-         </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Update Profile
-            </Button>
-          </Form.Item>
-        </Form>
-      )}
-    </div>
+          {loading ? (
+            <Spin />
+          ) : (
+            <Form
+              form={form}
+              layout="vertical"
+              onFinish={handleUpdate}
+              initialValues={{
+                name: user.name,
+                phone: user.phone,
+                mail: user.mail,
+                birthday: dayjs(user.birthday)
+              }}
+            >
+              <Form.Item
+                name="name"
+                label={<span><UserOutlined /> Name <span style={{ color: 'red' }}>*</span></span>}
+                rules={[{ required: true, message: "Please enter your name" }]}
+              >
+                <Input placeholder="Enter your name" />
+              </Form.Item>
+              <Form.Item
+                name="phone"
+                label={<span><PhoneOutlined /> Phone <span style={{ color: 'red' }}>*</span></span>}
+                rules={[
+                  { required: true, message: "Please enter your phone number" },{
+                    pattern: /^\d{10}$/,
+                    message: "Phone number must be 10 digits!",
+                  },]}
+              >
+                <Input placeholder="Enter your phone number" />
+              </Form.Item>
+              <Form.Item
+                name="mail"
+                label={<span><MailOutlined /> Email <span style={{ color: 'red' }}>*</span></span>}
+                rules={[{ required: true, message: "Please enter your email" }]}
+              >
+                <Input disabled />
+              </Form.Item>
+              <Form.Item
+                name="birthday"
+                label={<span><CalendarOutlined /> Birthday <span style={{ color: 'red' }}>*</span></span>}
+                rules={[{ required: true, message: "Please select your birthday" }]}
+              >
+                <DatePicker style={{ width: "100%" }}/>
+              </Form.Item>
+              <Form.Item>
+                <a href="/forgot">Wanna change password?</a>
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
+                  Update Profile
+                </Button>
+              </Form.Item>
+            </Form>
+          )}
+        </Card>
+      </div>
     </>
   );
 };
